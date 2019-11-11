b23035eb5e12a131a43970b5fa77fbfa871fd9d8
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-11 00:21:43 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(input)
+    @name = name
+    @type = type
+    @booty = booty
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
 end
